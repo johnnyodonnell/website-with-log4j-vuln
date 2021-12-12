74302648660e572f8e532128b187607da8693841
Johnny O'Donnell <odjohnny@amazon.com>
2021-12-12 00:15:15 -0800

    Save orders to csv file

--- src/main/java/com/cookies/website/MainController.java
+++ src/main/java/com/cookies/website/MainController.java
@@ -1,5 +1,9 @@
 package com.cookies.website;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -20,6 +24,22 @@ public class MainController {
     public void submit(
             HttpServletResponse response,
             @RequestParam Map<String, String> body) throws Exception {
+        String name = body.get("name");
+        String qty = body.get("qty");
+        String cc = body.get("cc");
+
+        String entry = String.join(",", name, qty, cc) + "\n";
+
+        String filename = "database.csv";
+
+        // Create file if it does not already exist
+        new File(filename).createNewFile();
+
+        Files.write(
+                Paths.get(filename),
+                entry.getBytes(),
+                StandardOpenOption.APPEND);
+
         response.sendRedirect("/success.html");
     }
 }
